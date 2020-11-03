9ca0fa023f7409f299e6d29b6bdb17b304693ba1
F354E270 <charlotte.entropy@gmail.com>
2020-11-03 17:48:42 -0600

    fh2362

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const expressLayouts = require('express-ejs-layouts'); // Express layouts
 const favicon = require('serve-favicon');
 const app = express();
 const path = require('path');
+const serverless = require('serverless-http');
 const {PythonShell} = require('python-shell');  
-const { request } = require('http');
-const extraTools = require('./custom_modules/extraTools');
 
 // Listen to a specific port OR :3000:
 const configPort = (process.env.PORT || 3000);
@@ -14,29 +13,36 @@ const configPort = (process.env.PORT || 3000);
 // Listen:
 app.listen(configPort, console.log(`Server started on ${configPort}`));
 
-app.use(favicon(path.join(__dirname, 'static', 'img', 'favicon.ico.png')));
+const router = express.Router();
 
-// Set the static route:
-app.use('/static', express.static(path.join(__dirname, '/static')));
-
-// EJS (Embedded JavaScript):
-app.use(expressLayouts); // Use the Express Layouts
 app.set('view engine', 'ejs'); // Use EJS View Engine
 app.set('views', path.join(__dirname, '/views')); // Directory for views:
 
-app.get('/', (request, response) => {
+router.get('/', (request, response) => {
     response.render('index');
 });
 
-app.get('/index', (request, response) => {
+router.get('/index', (request, response) => {
     response.render('index');
 });
 
-app.get('/robots.txt', (request, response) => { // https://stackoverflow.com/a/20265155
+router.get('/robots.txt', (request, response) => { // https://stackoverflow.com/a/20265155
     response.type('text/plain');
     response.sendFile('robots.txt', { root: __dirname });
 });
 
-app.get('*', (request, response) => { // https://stackoverflow.com/a/6528951
+router.get('*', (request, response) => { // https://stackoverflow.com/a/6528951
     response.sendFile('/static/html/50x.html', { root: __dirname }); // https://stackoverflow.com/a/26079640
 });
+
+app.use(favicon(path.join(__dirname, 'static', 'img', 'favicon.ico.png')));
+
+// Set the static route:
+app.use('/static', express.static(path.join(__dirname, '/static')));
+app.use('/.netlify/functions/server', router);
+
+// EJS (Embedded JavaScript):
+app.use(expressLayouts); // Use the Express Layouts
+
+module.exports = app;
+module.exports.handler = serverless(app);
